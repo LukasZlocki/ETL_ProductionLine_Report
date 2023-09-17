416aac24c90dbd22a16166f483a92183947d8496
lzlocki <lzlocki@poczta.fm>
2023-09-17 12:28:43 +0200

    Add empty class for data structure transformation

---
+++ DataTranformation/IStructureTransform.cs
@@ -0,0 +1,7 @@
+﻿namespace ETL_ProductionLine_Report.DataTranformation
+{
+    internal interface IStructureTransform
+    {
+        public List<List<string>> ConvertCsvToListOfListsStructure(List<string> dataset);
+    }
+}
---
+++ DataTranformation/StructureTransform.cs
@@ -0,0 +1,11 @@
+﻿namespace ETL_ProductionLine_Report.DataTranformation
+{
+    internal class StructureTransform : IStructureTransform
+    {
+        public List<List<string>> ConvertCsvToListOfListsStructure(List<string> dataset)
+        {
+            // ToDo: Write unit test for the class
+            throw new NotImplementedException();
+        }
+    }
+}
