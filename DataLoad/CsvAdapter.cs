97c5838d9d8119a523f8eb35bc05a1d9d1d950bb
lzlocki <lzlocki@poczta.fm>
2023-09-18 21:48:40 +0200

    Add class to save/load to csv file format

---
+++ DataLoad/CsvAdapter.cs
@@ -0,0 +1,15 @@
+﻿namespace ETL_ProductionLine_Report.DataLoad
+{
+    internal class CsvAdapter : ICsvAdapter
+    {
+        public List<string[]> LoadDataset(string pathName, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveDataset(string pathName, string fileName, List<string[]> dataSet)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataLoad/ICsvAdapter.cs
@@ -0,0 +1,8 @@
+﻿namespace ETL_ProductionLine_Report.DataLoad
+{
+    internal interface ICsvAdapter
+    {
+        public void SaveDataset(string pathName, string fileName, List<string[]> dataSet);
+        public List<string[]> LoadDataset(string pathName, string fileName);
+    }
+}
