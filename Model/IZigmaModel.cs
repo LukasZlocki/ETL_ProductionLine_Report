2043a26211ed550d0af5a526023e61a3318b489d
lzlocki <lzlocki@poczta.fm>
2023-09-18 22:10:37 +0200

    Add main datamodel and its interface

---
+++ Model/IZigmaModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETL_ProductionLine_Report.Model
+{
+    internal interface IZigmaModel
+    {
+        public List<string[]> GetDataset();
+        public void ChangeDataset(List<string[]> newDataset);
+        public void PrintDataset(List<string> dataset, int rowsToPrint);
+        public void PrintDataset(List<string> dataset);
+    }
+}
---
+++ Model/ZigmaModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETL_ProductionLine_Report.Model
+{
+    internal class ZigmaModel : IZigmaModel
+    {
+        private List<string[]> Model = new List<string[]>();
+
+        public void ChangeDataset(List<string[]> newDataset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string[]> GetDataset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintDataset(List<string> dataset, int rowsToPrint)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintDataset(List<string> dataset)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
