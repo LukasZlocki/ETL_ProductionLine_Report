b789091b22d92726bfb5db5c0f8af2d280b8d389
lzlocki <lzlocki@poczta.fm>
2023-09-19 21:27:23 +0200

    Remove folder with unnecessery files

--- DataLoad/ICsvAdapter.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ETL_ProductionLine_Report.DataLoad
-{
-    internal interface ICsvAdapter
-    {
-        public void SaveDataset(string pathName, string fileName, List<string[]> dataSet);
-        public List<string[]> LoadDataset(string pathName, string fileName);
-    }
-}
