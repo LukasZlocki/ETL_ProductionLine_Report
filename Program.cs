410034114fa986c71e373e148c569479a63aed64
lzlocki <lzlocki@poczta.fm>
2023-10-02 15:44:04 +0200

    Add code to revome column

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿using Zigma.ExtractionTools;
 using Zigma.Models;
+using Zigma.TransformationTools;
 
 // ToDo - Plan
 // ToDo: Get data from raw csv
 // ToDo: Transform column with date to date format yyyy/mm/dd
 // ToDo: Create daily report with summary of units planned / output - daily report / weekly report - ! extend zigma to perform this activities ! 
 // ToDo ZIGMA | TransformTool | Add method to create new column (add new column at the end of dataset)
-// ToDo ZIGMA | TransformTool | Add method to remove column
 // ToDo Zigma | TransformTool | Add method to move particular column to new possition ex: no move column nb 5 to 3) rest columns will move +1 column (ColumnMove(a, c)) 
 // ToDo Zigma | TransformTool | Add method to switch columns ex: switch column 3 with column 2 (ColumnSwitch(a, b))
 // ToDo Zigma | TransformTool | Add method to divide date by day / week / month and sign what to do with rest of columns (+, -, *, /)
@@ -14,11 +14,18 @@ using Zigma.Models;
 
 // STEP I : Extracting data from csv row file
 ExtractionTool extraction = new();
-ZigmaModel model = new ZigmaModel();
+ZigmaModel model = new();
+TransformationTool transform = new();
 
 string filePath = "C:\\0 VirtualServer\\ETL\\";
 string fileName = "raw_raports.csv";
 model.CreateZigmaDataset(extraction.LoadFromCsvFile(filePath, fileName));
 // Printing dataset
 model.PrintZigmaDataset(10);
-model.PrintZigmaDataset(1); 
+
+// ToDo ZIGMA | TransformTool | Add method to remove column
+// ToDo ZIGMA | TransformTool | Add method to remove column
+ZigmaModel NewModelColumnRemoved = new();
+NewModelColumnRemoved = transform.ColumnRemove(model, 0);
+Console.WriteLine("Print dataset with removed column");
+NewModelColumnRemoved.PrintZigmaDataset(10);
