648bb6dd59c9c13e897e7b926c0336f545064dcc
lzlocki <lzlocki@poczta.fm>
2023-10-06 18:21:13 +0200

    Add methods responsible for calculationg daily reports

--- Services/ReportServices.cs
+++ Services/ReportServices.cs
@@ -3,6 +3,7 @@ using ETL_ProductionLine_Report.Models;
 namespace ETL_ProductionLine_Report.Services {
     public class ReportService : IReportService
     {
+        // [06.10.2023] - InProgress
         public List<ReportDaily> GetListOfDailyReportsBaseOnDataset(List<string[]> dataset, int columnWithDate)
         {
             List<string> listOfDates = GetListOfDatesFromDataset(dataset, columnWithDate);
@@ -10,26 +11,38 @@ namespace ETL_ProductionLine_Report.Services {
             return dailyReportsList;
         }
 
+        //[06.10.2023]  - Done
         public ReportDaily GetReportForGivenDay(List<string[]> dataset, string day, int columnNumberWithDate)
         {
-            List<string[]> _dayData = new();
+            List<string[]> _dayData;
             _dayData = ExtractDatasetForGivenDay(dataset, day, columnNumberWithDate);
+            ReportDaily _dailyReportBuild = new();
+            foreach(string[] element in _dayData) {
+                int _plannedOutput = Convert.ToInt32(_dailyReportBuild.PlannedOutput) + Convert.ToInt32(element[2]);
+                _dailyReportBuild.PlannedOutput =  Convert.ToString(_plannedOutput);
+                int _realOutput = Convert.ToInt32(_dailyReportBuild.RealOutput) + Convert.ToInt32(element[3]);
+                _dailyReportBuild.RealOutput = Convert.ToString(_realOutput);
+            }
+            // Calculation of planned production to real production
+            double _plannedPercentage = Convert.ToDouble(_dailyReportBuild.RealOutput) / Convert.ToDouble(_dailyReportBuild.PlannedOutput);
+            _dailyReportBuild.PlanedPercentage = Convert.ToString(_plannedPercentage);
+            // Add user date to daily report
+            _dailyReportBuild.Date = day;
 
-            foreach (string[] row in dataset)
-            {
-                if (row[columnNumberWithDate] == day)
-                {
+            return _dailyReportBuild;
+        }
+
+        private List<string[]> ExtractDatasetForGivenDay(List<string[]> dataset, string day, int columnNumberWithDate) {
+            List<string[]> _dayData = new();
+            foreach(string[] row in dataset) {
+                if (row[columnNumberWithDate] == day) {
                     _dayData.Add(row);
                 }
-                else
-                {
+                else {
                     continue;
                 }
             }
-
-            ReportDaily _dailyReport = new();
-            // ToDo: Code logic here
-            return _dailyReport;
+            return _dayData;
         }
 
         private List<ReportDaily> GetListOfDailyReports(List<string[]> dataset, List<string> dates, int columnNumberWithDate){
@@ -39,23 +52,6 @@ namespace ETL_ProductionLine_Report.Services {
                 _listOfDailyReports.Add(_newReport);
             }
             return _listOfDailyReports;
-        }
-
-        private List<string[]> ExtractDatasetForGivenDay(List<string[]> dataset, string day, int columnNumberWithDate)
-        {
-            List<string[]> _dayData = new();
-            foreach (string[] row in dataset)
-            {
-                if (row[columnNumberWithDate] == day)
-                {
-                    _dayData.Add(row);
-                }
-                else
-                {
-                    continue;
-                }
-            }
-            return _dayData;
         }
 
         public List<string> GetListOfDatesFromDataset(List<string[]> dataset, int columnNumberWithDate)
